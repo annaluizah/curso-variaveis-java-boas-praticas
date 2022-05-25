63f3376eb364cc4020378f48a99a17313f3be99a
Anna Luiza Huff <77989116+annaluizah@users.noreply.github.com>
2022-05-25 20:19:18 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,52 @@
+package boasPraticas;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i;
+        // inválido -> int i;
+        int I;
+        // inválido -> int 1a;
+        int _1a;
+        int $aq;
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+        final int j = 10;
+        // inválido -> j = 15;
+        int asrn24678md;
+        // inválido -> int asrn246 78md;
+        int asrn2$4678_md = 10;
+        // inválido -> int asrn2$46%78_md = 10;
+
+        asrn24678md = 100;
+        asrn2$4678_md = 10;
+
+        int quantidadeProduto = 50;
+        // não é boa prática -> int QuantidadeProduto;
+        final int NUMERO_TENTATIVAS = 5;
+        // não é boa prática -> final int numeroTentativas = 5;
+        int QUANTIDADE_OPCOES = 25;
+        // não é boa prática -> int qtdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(asrn24678md);
+        System.out.println(asrn2$4678_md);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+
+
+    }
+
+}
